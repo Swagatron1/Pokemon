85fa08a7f35092ef48f2c1913a6025c743782add
herridav000 <herridav000@739CTE134LAB18>
2018-04-12 08:46:37 -0600

    New Project Changed Names

---
+++ ConsoleProject1/ConsoleProject1/Program.cs
@@ -0,0 +1,29 @@
+﻿// File/Project Prolog
+// Name:     <David Herring and Caleb Norton>
+// Period:   <PUT YOUR PERIOD HERE>
+// Username: herridav000
+// Project:  ConsoleProject1
+// Date:     4/12/2018 8:42:10 AM
+//
+// I declare that the following code was written by me or provided 
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+// ---------------------------------------------------------------------------
+
+using System;
+
+namespace ConsoleProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Your code goes below here
+
+            // Your code goes above here
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
