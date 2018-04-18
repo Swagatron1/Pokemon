75a5873e06deea63e6e499e866562c410994e691
herridav000 <herridav000@739CTE134LAB18>
2018-04-18 09:08:43 -0600

    attack class

--- ConsoleProject1/ConsoleProject1/Pokemon.cs
+++ ConsoleProject1/ConsoleProject1/Pokemon.cs
@@ -55,7 +55,22 @@ namespace ConsoleProject1
         {
             return Enemyhealth - 30;
         }
+        {
+        public string enemyname;
+        public int enemyhealth = 100;
+        public int enemyattack1;
+        public int enemyattack2;
+        public int enemyattack3;
+        public int enemyattack4;
 
+        public Enemy(string enemyname, int enemyhealth, int enemyattack1, int enemyattack2, int enemyattack3)
+        {
+            this.enemyname = enemyname;
+            this.enemyhealth = enemyhealth;
+            this.enemyattack1 = enemyattack1;
+            this.enemyattack2 = enemyattack2;
+            this.enemyattack3 = enemyattack3;
+        }
 
 
 
