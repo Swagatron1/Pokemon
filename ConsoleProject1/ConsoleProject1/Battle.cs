526acc2b2360f6d5238d30c15e96c822739d800e
herridav000 <herridav000@739CTE134LAB18>
2018-05-24 08:12:41 -0600

    created battle class, working on that

--- ConsoleProject1/ConsoleProject1/Battle.cs
+++ ConsoleProject1/ConsoleProject1/Battle.cs
@@ -7,7 +7,18 @@ using System.Threading.Tasks;
 namespace ConsoleProject1
 {
     class Battle
-    {
+    {//Battle Fields
+        public int health;
+        public int damage;
+        public int attackdamage;
+
+        public Battle(int health, int damage, int attackdamage)
+        {
+            this.health = health;
+            this.damage = damage;
+            this.attackdamage = attackdamage;
+        }
+
 
     }
 }
